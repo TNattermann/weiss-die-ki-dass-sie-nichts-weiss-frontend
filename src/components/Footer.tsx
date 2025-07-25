ebfde256dc7add81a228376a4e47b7a44821e608
n_urbansky21 <n_urbansky21@cs.uni-kl.de>
2025-07-25 17:32:38 +0200

    added dark mode color for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer className="bg-slate-600 pb-8 pt-12">
+        <footer className="bg-footer pb-8 pt-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <div>
