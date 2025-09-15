d6f3037d64f0479d6b1d4cd9377c6437d842497e
Cameron Strenge <strenge@rhrk.uni-kl.de>
2025-09-15 16:52:25 +0200

    Tilde upgedated

--- src/components/Tilde.tsx
+++ src/components/Tilde.tsx
@@ -1,25 +1,23 @@
 
 
 export default function Tilde(){
+
+    const tildetext = `Short Version: Die Tilde-Notation (~) wird benutz  für Wörter,
+    die menschliches Verhalten suggerieren, obwohl dies so nicht auf Modelle zutrifft.
+    
+    Long Version: Wir verwenden die Tilde-Notation (~) für Verhalten des Computers
+    (also Sprachmodelle, aber auch KI im allgemeinen), die menschliches Verhalten suggerieren,
+    aber diese Tätigkeit im eigentlichen Sinne nicht wiederspiegeln.
+    "Der Computer ~versteht mich, ~fasst Texte ~zusammen und
+     schreibt ~Bewertungen. 
+    Nichts davon ist leistungsgleich zu dem, was der Mensch tut", Zweig, Weiß die KI, dass sie nichts weiß? 2025
+    Dennoch ist die Tätigkeit, die der Computer ausübt, im Äußeren ähnlich zu dem, 
+    was ein Mensch darunter erwarten würde.`;
     
     return(
 
-        <span className="hover">
-            <span className="bg-primary/5 border-l-4 text-primary p-6 rounded-r-lg mb-6 text-textMedium font-medium">
-                Short Version: Die Tilde-Notation (~) wird benutz  für Wörter,
-                die menschliches Verhalten suggerieren, obwohl dies so nicht auf Modelle zutrifft.
-                
-                Long Version: Wir verwenden die Tilde-Notation (~) für Verhalten des Computers
-                (also Sprachmodelle, aber auch KI im allgemeinen), die menschliches Verhalten suggerieren,
-                aber diese Tätigkeit im eigentlichen Sinne nicht wiederspiegeln. "
-                "Der Computer ~versteht mich, ~fasst Texte ~zusammen und
-                 schreibt ~Bewertungen. 
-                Nichts davon ist leistungsgleich zu dem, was der Mensch tut", TBD Zweig Zitat 
-                Dennoch ist die Tätigkeit, die der Computer ausübt, im Äußeren ähnlich zu dem, 
-                was ein Mensch darunter erwarten würde.
-            </span>
-        ~<sup>i</sup>
-        </span>
+        <span data-tooltip={tildetext}>~<sup>i</sup></span>
+        
         
     );
 
