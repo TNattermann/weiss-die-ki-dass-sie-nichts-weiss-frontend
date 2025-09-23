60bc1899921743ec2bb7dd9f3bd03ee2ffc27d31
Yannik Höreth <y_hoereth16@informatik.uni-kl.de>
2025-09-23 14:14:06 +0200

    Added Bit Intro

--- src/pages/Bits/WissenPage.tsx
+++ src/pages/Bits/WissenPage.tsx
@@ -8,7 +8,11 @@ function WissenPage() {
         <h1 className="text-5xl font-bold text-center text-primary mb-4">
           Wissenshäppchen
         </h1>
-
+        <p className="text-lg text-text-normal mb-6 leading-relaxed">
+          Hier werden die wichtigsten Erkenntnisse und Informationen von der
+          Webseite übersichtlich dargestelt. Wissenshäppchen - perfekt zum
+          nachschlagen.
+        </p>
         <Link to="/attention#attention1">
           <div className="bg-primary/5 border-l-4 text-primary p-6 rounded-r-lg mb-6">
             {bits.slice(0, 1).map((c) => (
