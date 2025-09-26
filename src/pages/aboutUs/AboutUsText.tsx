98dbf15c68dfa3d21fa032c641b10c2c2e65e472
Yannik Höreth <y_hoereth16@informatik.uni-kl.de>
2025-09-26 14:48:02 +0200

    about us picture

--- src/pages/aboutUs/AboutUsText.tsx
+++ src/pages/aboutUs/AboutUsText.tsx
@@ -6,13 +6,13 @@ export default function AboutUsText() {
   }, []);
   return (
     <div className="max-w-screen-xl mx-auto px-6 py-6 text-center">
-      <div className="items-center mb-16">
+      <div className="items-center mb-4">
         <p className="text-lg text-text-normal mb-6 leading-relaxed">
           Wir sind eine Gruppe von Studierenden, die ihren Master in
           Sozioinformatik an der RPTU in Kaiserslautern machen. Diese Webseite
           ist im Rahmen unseres Master-Projekts entstanden und ergänzt das Buch
-          "Weiß die KI, dass sie nichts weiß?" von Prof. Dr. Katharina A. Zweig. Unser
-          Ziel ist es, allen Menschen ein grundlegendes Verständnis von
+          "Weiß die KI, dass sie nichts weiß?" von Prof. Dr. Katharina A. Zweig.
+          Unser Ziel ist es, allen Menschen ein grundlegendes Verständnis von
           Sprachmodellen zu vermitteln, unabhängig von technischen
           Vorkenntnissen.
         </p>
